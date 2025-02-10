b753b702d7c0ce841c8e6f7883c87de0e4c5e6a1
231055558 <231055558@qq.com>
2025-02-10 12:20:15 +0800

    小bug修复

--- models/resnet/resnet50_224_bce_adam_lr1e-3_bs32.py
+++ models/resnet/resnet50_224_bce_adam_lr1e-3_bs32.py
@@ -9,70 +9,77 @@ from dataset.A007_txt import A007Dataset
 from dataset.transform import *
 from torch.utils.data import DataLoader
 
+def main(mode):
 
-data_root = '/mnt/mydisk/medical_seg/fwwb_a007/data/dataset'
-transform_train = Compose([LoadImageFromFile(),
-                           Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375)),
-                           RandomCrop((512, 512)),
-                           Resize((512, 512)),
-                           ToTensor()])
+    data_root = '../../../data/dataset'
+    transform_train = Compose([LoadImageFromFile(),
+                               Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375)),
+                               RandomCrop((512, 512)),
+                               Resize((512, 512)),
+                               ToTensor()])
 
-transform_test = Compose([LoadImageFromFile(),
-                          Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375)),
-                          CenterCrop((512, 512)),
-                          Resize((512, 512)),
-                          ToTensor()])
+    transform_test = Compose([LoadImageFromFile(),
+                              Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375)),
+                              CenterCrop((512, 512)),
+                              Resize((512, 512)),
+                              ToTensor()])
 
-model = ResNet(depth=50,
-               num_classes=8)
-ckp = "../../../checkpoints/resnet50.pth"
+    model = ResNet(depth=50,
+                   num_classes=8)
 
-load_model_weights(model, ckp)
 
-train_loader = DataLoader(A007Dataset(txt_file="train.txt",
-                                      root_dir="/mnt/mydisk/medical_seg/fwwb_a007/data/dataset",
-                                      transform=transform_train,
-                                      seed=42,
-                                      preload=False),
-                          batch_size=32,
-                          shuffle=True,
-                          num_workers=16,
-                          pin_memory=True
+    train_loader = DataLoader(A007Dataset(txt_file="train.txt",
+                                          root_dir=data_root,
+                                          transform=transform_train,
+                                          seed=42,
+                                          preload=False),
+                              batch_size=32,
+                              shuffle=True,
+                              num_workers=16,
+                              pin_memory=True
+                              )
+    val_loader = DataLoader(A007Dataset(txt_file="val.txt",
+                                          root_dir=data_root,
+                                          transform=transform_test,
+                                          seed=42,
+                                          preload=False),
+                              batch_size=32,
+                              shuffle=True,
+                              num_workers=16,
+                              pin_memory=True
+                              )
+    loss_fn = CrossEntropyLoss(use_sigmoid=True)
+    metric = A007_Metrics(thresholds=[0.1, 0.3, 0.5, 0.7, 0.9])
+    optimizer = Optimizer(model_params=model.parameters(),
+                          optimizer='adam',
+                          lr=1e-3,
+                          weight_decay=1e-4
                           )
-val_loader = DataLoader(A007Dataset(txt_file="train.txt",
-                                      root_dir="/mnt/mydisk/medical_seg/fwwb_a007/data/dataset",
-                                      transform=transform_test,
-                                      seed=42,
-                                      preload=False),
-                          batch_size=32,
-                          shuffle=True,
-                          num_workers=16,
-                          pin_memory=True
-                          )
-loss_fn = CrossEntropyLoss(use_sigmoid=True)
-metric = A007_Metrics(thresholds=[0.1, 0.3, 0.5, 0.7, 0.9])
-optimizer = Optimizer(model_params=model.parameters(),
-                      optimizer='adam',
-                      lr=1e-3,
-                      weight_decay=1e-4
-                      )
+    if mode == "train":
+        pretrain_ckp = "../../../checkpoints/resnet50.pth"
+        load_model_weights(model, pretrain_ckp)
+        train_model(
+            model=model,
+            train_loader=train_loader,
+            val_loader=val_loader,
+            loss_fn=loss_fn,
+            metric=metric,
+            optimizer=optimizer,
+            device='cuda',
+            num_epochs=100,
+            save_path='best_model.pth',
+            val=True
+        )
+    if mode == "val":
+        trained_ckp = "../../../checkpoints/resnet50_224_bce_adam_lr1e-3_bs32_checkpoint/best_model.pth"
+        load_model_weights(model, trained_ckp)
+        val_model(
+            model=model,
+            val_loader=val_loader,
+            metric=metric,
+            device='cuda'
+        )
 
-train_model(
-    model=model,
-    train_loader=train_loader,
-    val_loader=val_loader,
-    loss_fn=loss_fn,
-    metric=metric,
-    optimizer=optimizer,
-    device='cuda',
-    num_epochs=100,
-    save_path='best_model.pth',
-    val=True
-)
-
-# val_model(
-#     model=model,
-#     val_loader=val_loader,
-#     metric=metric,
-#     device='cuda'
-# )
+if __name__ == '__main__':
+    mode = "val"
+    main(mode)
