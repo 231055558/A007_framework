e1c35d3f049e034dfa51ae3de2197db3b5cd9f21
李昊洋 <231055558@qq.com>
2025-03-18 01:35:06 +0800

    加入了resnet_double方案

---
+++ models/resnet_double/resnet50_double_merge_768_bce_adam_lr1e-2_bs32.py
@@ -0,0 +1,120 @@
+from models.load import load_model_weights
+from networks.resnet_to_linear import ResNet_To_Linear
+from blocks.head import AttentionFC
+from tools.predict import predict_model
+from tools.train import train_double_merge_model
+from tools.val import val_output_merge_model
+from loss.cross_entropy import CrossEntropyLoss
+from metrics.a007_metric import A007_Metrics_Label
+from optims.optimizer import Optimizer
+from dataset.A007_txt_merge_model import A007Dataset
+from dataset.transform import *
+from torch.utils.data import DataLoader
+from visualization.visualizer import Visualizer
+
+
+class ResNet50_Double_Merge_768_Bce_Adam_Lr1e_2_Bs32:
+    def __init__(self):
+        self.data_root = '../../../data/data_merge'
+        self.pretrain_ckp_model_1 = "../../../checkpoints/resnet50.pth"
+        self.pretrain_ckp_model_2 = "../../../checkpoints/resnet50.pth"
+        self.pretrain_ckp_head = None
+        self.model_name = 'ResNet50_224_Bce_Adam_Lr1e_3_Bs32'
+        self.transform_train = Compose([LoadImageFromFile(),
+                                        RandomFlip(),
+                                        RandomCrop((256, 256)),
+                                        ToTensor(),
+                                        Resize((256, 256)),
+                                        Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375))])
+
+        self.transform_val = Compose([LoadImageFromFile(),
+                                      CenterCrop((256, 256)),
+                                      ToTensor(),
+                                      Resize((256, 256)),
+                                      Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375))])
+        self.model_1 = ResNet_To_Linear(depth=50,
+                            num_classes=8)
+        self.model_2 = ResNet_To_Linear(depth=50,
+                            num_classes=8)
+        self.head = AttentionFC(in_features=4096, num_classes=8)
+
+        self.train_loader = DataLoader(A007Dataset(txt_file="train.txt",
+                                                   root_dir=self.data_root,
+                                                   transform=self.transform_train,
+                                                   seed=42,
+                                                   preload=False),
+                                       batch_size=16,
+                                       shuffle=True,
+                                       num_workers=4,
+                                       pin_memory=True
+                                       )
+        self.val_loader = DataLoader(A007Dataset(txt_file="val.txt",
+                                                 root_dir=self.data_root,
+                                                 transform=self.transform_val,
+                                                 seed=42,
+                                                 preload=False),
+                                     batch_size=16,
+                                     shuffle=False,
+                                     num_workers=4,
+                                     pin_memory=True
+                                     )
+        self.loss_fn = CrossEntropyLoss(use_sigmoid=True)
+        self.metric = A007_Metrics_Label(thresholds=[0.1, 0.3, 0.5, 0.7, 0.9])
+        self.optimizer = Optimizer(model_params=self.model.parameters(),
+                                   optimizer='adam',
+                                   lr=1e-3,
+                                   weight_decay=1e-4
+                                   )
+        self.visualizer = Visualizer(experiment_name=self.model_name, metrics=self.metric)
+        #self.pretrain_ckp = "./best_model.pth"
+
+    def train(self, epoch=100, val=True):
+        load_model_weights(self.model_1, self.pretrain_ckp_model_1)
+        load_model_weights(self.model_2, self.pretrain_ckp_model_2)
+        if self.pretrain_ckp_head is not None:
+            load_model_weights(self.head, self.pretrain_ckp_head)
+            
+        train_double_merge_model(
+            model_1=self.model_1,
+            model_2=self.model_2,
+            head=self.head,
+            model_name=self.model_name,
+            train_loader=self.train_loader,
+            val_loader=self.val_loader,
+            loss_fn=self.loss_fn,
+            metric=self.metric,
+            optimizer=self.optimizer,
+            device='cuda',
+            num_epochs=epoch,
+            save_path='best_model.pth',
+            val=val,
+            visualizer=self.visualizer
+        )
+
+    def val(self):
+        trained_ckp = "./best_model.pth"
+        load_model_weights(self.model, trained_ckp)
+        val_output_merge_model(
+            model=self.model,
+            model_name=self.model_name,
+            val_loader=self.val_loader,
+            metric=self.metric,
+            device='cuda'
+        )
+
+    def predict_model(self):
+        trained_ckp = "./best_model.pth"
+        load_model_weights(self.model, trained_ckp)
+        predict_model(
+            model=self.model,
+            test_loader=self.val_loader,
+            metric=self.metric,
+            model_name=self.model_name,
+            device='cuda',
+            output_folder="./output"
+        )
+
+
+if __name__ == '__main__':
+    model = ResNet50_Double_Merge_768_Bce_Adam_Lr1e_2_Bs32()
+    model.val()
--- tools/train.py
+++ tools/train.py
@@ -363,3 +363,108 @@ def train_net_merge_model(
                 print(f'Saved best model with combined metrics (ACC+PREC+REC)/3: {best_metrics:.4f}')
 
     print("Training finished")
+
+def train_double_merge_model(
+        model_1,
+        model_2,
+        head,
+        train_loader,
+        loss_fn,
+        optimizer,
+        visualizer,
+        device='cuda',
+        model_name="default_model",
+        num_epochs=100,
+        save_path='best',
+        val=False,
+        val_loader=None,
+        metric=None
+):
+    visualizer.log("-----------start training--------------")
+    if val:
+        assert val_loader is not None and metric is not None
+    model_1.to(device)
+    model_2.to(device)
+    best_metrics = float('0')
+
+    for epoch in range(num_epochs):
+        model_1.train()
+        model_2.train()
+        running_loss = 0.0
+        progress_bar = tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}")
+
+        for inputs_l, inputs_r, labels in progress_bar:
+            time_start = time.time()
+
+            inputs_l = inputs_l.to(device)
+            inputs_r = inputs_r.to(device)
+            labels = labels.to(device)
+
+            # 前向传播
+            outputs_l = model_1(inputs_l)
+            outputs_r = model_2(inputs_r)
+            output_linear = torch.cat((outputs_l, outputs_r), dim=1)
+            outputs = head(output_linear)
+
+            # 计算损失
+            loss = loss_fn(outputs, labels)
+
+            # 反向传播
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # 记录损失和时间
+            running_loss += loss.item()
+            time_cost = time.time() - time_start
+            progress_bar.set_postfix(loss=loss.item(), time_cost=time_cost)
+
+            # 释放不需要的张量
+            del outputs_l, outputs_r, outputs, loss
+
+            # 清除 PyTorch 缓存
+            torch.cuda.empty_cache()
+
+        epoch_loss = running_loss / len(train_loader)
+        visualizer.log(f'Epoch {epoch + 1}/{num_epochs} loss: {epoch_loss:.4f}')
+        visualizer.update_loss(epoch_loss)
+
+        # print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
+        # if epoch_loss < best_loss:
+        #     best_loss = epoch_loss
+        #     torch.save(model.state_dict(), save_path)
+        #     print(f'Saved best model with loss {epoch_loss:.4f}')
+        if val:
+            # 计算验证指标
+            metrics = val_output_merge_model(model, val_loader, metric, model_name, device)
+
+            # 更新可视化图表
+            visualizer.update_metrics(metrics)
+
+            # 新增：将指标详细信息写入日志
+            visualizer.log_metrics(metrics)
+
+            # 保存最佳模型
+            mean_metrics = 0
+            num_thresholds = 0
+            for thr in metrics:
+                num_thresholds += 1
+                # 计算三个指标的平均值
+                current_metrics = (
+                    metrics[thr]["overall_accuracy"] + 
+                    metrics[thr]["overall_precision"] + 
+                    metrics[thr]["overall_recall"]
+                ) / 3.0
+                mean_metrics += current_metrics
+                
+            # 计算所有阈值下的平均综合指标
+            avg_metrics = mean_metrics / num_thresholds
+            if avg_metrics > best_metrics:  # best_metrics需要在训练开始前初始化为0
+                best_metrics = avg_metrics
+                torch.save(model_1.state_dict(), save_path + "_model_1.pth")
+                torch.save(model_2.state_dict(), save_path + "_model_2.pth")
+                torch.save(head.state_dict(), save_path + "_head.pth")
+                print(f'Saved best model with combined metrics (ACC+PREC+REC)/3: {best_metrics:.4f}')
+
+    print("Training finished")
+
