6bc1ac9a804e39c04aa5da0136f97e8080368dac
李昊洋 <231055558@qq.com>
2025-03-17 18:12:56 +0800

    为deeplabv3+, resnet50, swin全部加入新的损失函数和head

---
+++ models/resnet_fpn_img_merge/resnet50_fpn_output_merge_768_bce_adam_lr1e-3_bs32.py
@@ -0,0 +1,116 @@
+from models.load import load_model_weights
+from networks.resent_fpn_attention_head import ResNetFPNAttentionHead
+from tools.predict import predict_model
+from tools.train import train_output_merge_model
+from tools.val import val_output_merge_model
+from loss.cross_entropy import CrossEntropyLoss
+from metrics.a007_metric import A007_Metrics_Label
+from optims.optimizer import Optimizer
+from dataset.A007_txt_merge_model import A007Dataset
+from dataset.transform import *
+from torch.utils.data import DataLoader
+from visualization.visualizer import Visualizer
+
+
+class ResNet50_Output_Merge_224_Bce_Adam_Lr1e_3_Bs32:
+    def __init__(self):
+        self.data_root = '../../../data/data_merge'
+        self.pretrain_ckp = "../../../checkpoints/resnet50.pth"
+
+        self.model_name = 'ResNet50_224_Bce_Adam_Lr1e_3_Bs32'
+        self.transform_train = Compose([LoadImageFromFile(),
+                                        RandomFlip(),
+                                        RandomCrop((256, 256)),
+                                        ToTensor(),
+                                        Resize((256, 256)),
+                                        Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375))])
+
+        self.transform_val = Compose([LoadImageFromFile(),
+                                      CenterCrop((256, 256)),
+                                      ToTensor(),
+                                      Resize((256, 256)),
+                                      Preprocess(mean=(123.675, 116.28, 103.53), std=(58.395, 57.12, 57.375))])
+        self.model = ResNetFPNAttentionHead(
+            depth=50,
+            in_channels=3,
+            stem_channels=64,
+            base_channels=64,
+            num_classes=8,
+            norm='batch_norm',
+            activation='relu'
+        )
+
+        self.train_loader = DataLoader(A007Dataset(txt_file="train.txt",
+                                                   root_dir=self.data_root,
+                                                   transform=self.transform_train,
+                                                   seed=42,
+                                                   preload=False),
+                                       batch_size=16,
+                                       shuffle=True,
+                                       num_workers=4,
+                                       pin_memory=True
+                                       )
+        self.val_loader = DataLoader(A007Dataset(txt_file="val.txt",
+                                                 root_dir=self.data_root,
+                                                 transform=self.transform_val,
+                                                 seed=42,
+                                                 preload=False),
+                                     batch_size=16,
+                                     shuffle=False,
+                                     num_workers=4,
+                                     pin_memory=True
+                                     )
+        self.loss_fn = CrossEntropyLoss(use_sigmoid=True)
+        self.metric = A007_Metrics_Label(thresholds=[0.1, 0.3, 0.5, 0.7, 0.9])
+        self.optimizer = Optimizer(model_params=self.model.parameters(),
+                                   optimizer='adam',
+                                   lr=1e-3,
+                                   weight_decay=1e-4
+                                   )
+        self.visualizer = Visualizer(experiment_name=self.model_name, metrics=self.metric)
+        #self.pretrain_ckp = "./best_model.pth"
+
+    def train(self, epoch=100, val=True):
+        load_model_weights(self.model, self.pretrain_ckp)
+        train_output_merge_model(
+            model=self.model,
+            model_name=self.model_name,
+            train_loader=self.train_loader,
+            val_loader=self.val_loader,
+            loss_fn=self.loss_fn,
+            metric=self.metric,
+            optimizer=self.optimizer,
+            device='cuda',
+            num_epochs=epoch,
+            save_path='best_model.pth',
+            val=val,
+            visualizer=self.visualizer
+        )
+
+    def val(self):
+        trained_ckp = "./best_model.pth"
+        load_model_weights(self.model, trained_ckp)
+        val_output_merge_model(
+            model=self.model,
+            model_name=self.model_name,
+            val_loader=self.val_loader,
+            metric=self.metric,
+            device='cuda'
+        )
+
+    def predict_model(self):
+        trained_ckp = "./best_model.pth"
+        load_model_weights(self.model, trained_ckp)
+        predict_model(
+            model=self.model,
+            test_loader=self.val_loader,
+            metric=self.metric,
+            model_name=self.model_name,
+            device='cuda',
+            output_folder="./output"
+        )
+
+
+if __name__ == '__main__':
+    model = ResNet50_Output_Merge_224_Bce_Adam_Lr1e_3_Bs32()
+    model.val()
